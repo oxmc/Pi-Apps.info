fc6aee6cba8bcf764b79c4835269c92a6495f077
oxmc <67136658+oxmc@users.noreply.github.com>
2021-01-17 23:47:20 -0800

    Shoutout to Itami on discord for fixing this script

--- files/Pi-AppsTerm_install.sh
+++ files/Pi-AppsTerm_install.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+#Variables
+
+message="Thanks to Itai on discord for helping me fix install errors!"
+
 #Functions
 function error {
   echo -e "\e[91m$1\e[39m"
@@ -7,11 +11,13 @@ function error {
 }
 
 #Main
+echo "${message}"
 sudo apt install git || error "Unable to install git!"
-git clone https:/github.com/oxmc/Pi-Apps.info.git || error "Unable to download Pi-AppsTerm!"
+git clone https://github.com/oxmc/Pi-Apps.info.git || error "Unable to download Pi-AppsTerm!"
 git clone https://github.com/Botspot/pi-apps.git || error "Unable to download pi-apps!"
-rename Pi-Apps.info PITM || error "Unable to change folder name!"
+mv Pi-Apps.info PITM || error "Unable to change folder name!"
 cd PITM || error "Unable to change directiry to PITM!"
 chmod +x *.* || error "Unable to change executable permissons!"
-cp files/Pi-AppsTerm /usr/bin/pi-apps || error "Unable to add Pi-Apps to terminal!"
+sudo cp files/Pi-AppsTerm /usr/bin/pi-apps || error "Unable to add Pi-Apps to terminal!"
+sudo chmod +x /usr/bin/pi-apps
 pi-apps version || error "Unable to run version command, pi-appsterm might be installed incorrectly!"
