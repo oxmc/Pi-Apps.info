019baf78bde969b324a1af13e741fe25d1026011
oxmc <67136658+oxmc@users.noreply.github.com>
2021-01-18 03:27:54 -0800

    Update Pi-AppsTerm_install.sh

--- files/Pi-AppsTerm_install.sh
+++ files/Pi-AppsTerm_install.sh
@@ -23,4 +23,5 @@ sudo chmod +x /usr/local/bin/pi-apps || error "Unable to change executable permi
 sudo rm -r index.html || error "Unable to delete non required files!"
 sudo rm -r stylesheet.css || error "Unable to delete non required files!"
 sudo rm -r README.md || error "Unable to delete non required files!"
+sudo rm -r images/*.* || error "Unable to delete non required files!"
 pi-apps version || error "Unable to run version command, pi-appsterm might be installed incorrectly!"
